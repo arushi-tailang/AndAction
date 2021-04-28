30939d5860685f376abda82a00567b06f39e9a37
Arushi Tailang <arushitailang2001@gmail.com>
2021-04-28 06:01:34 +0530

    my movies commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Header.css'
+
+function Header() {
+    return <span onClick={() => window.scroll(0,0)} className="header"> And ACTION</span>
+        
+    
+}
+
+export default Header
